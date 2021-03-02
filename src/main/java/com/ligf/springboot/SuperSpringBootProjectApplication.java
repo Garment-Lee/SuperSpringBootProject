9db777e340b317bf2da4ed5bfb92bd4d60a048a2
garmentlee <garment1991@163.com>
2021-03-02 18:23:45 +0800

    add the project.

---
+++ src/main/java/com/ligf/springboot/SuperSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ligf.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperSpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperSpringBootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ligf/springboot/SuperSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ligf.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
